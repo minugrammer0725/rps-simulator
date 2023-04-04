b445886ecffe7525744fb50a802d563d45170418
minugrammer0725 <kwawkminoo@gmail.com>
2023-04-04 12:52:57 -0700

    set up Button class

---
+++ buttons.py
@@ -0,0 +1,26 @@
+from turtle import Turtle
+
+class Button(Turtle):
+    def __init__(self, shape, wid, len, color, x, y, onclick):
+        super().__init__(shape)
+        self.shapesize(wid, len)
+        self.color(color)
+        self.onclick(onclick)
+        self.penup()
+        self.goto(x, y)
+
+
+class ControlButton(Button):
+    def __init__(self, shape, wid, len, color, x, y, label_y, onclick):
+        super().__init__(shape, wid, len, color, x, y, onclick)
+        self.toggle = False 
+        
+    def writeSome():
+        pass 
+        
+class ReverseButton(Button):
+    def __init__(self, shape, wid, len, color, x, y, onclick, angle, width):
+        super().__init__(shape, wid, len, color, x, y, onclick)
+        self.right(angle)
+        self.width(width)
+

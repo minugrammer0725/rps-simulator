ad5dacb5beb2307aad7bb58851fb7e910e92638f
minugrammer0725 <kwawkminoo@gmail.com>
2023-04-04 13:20:39 -0700

    fixed control button class

--- buttons.py
+++ buttons.py
@@ -11,12 +11,13 @@ class Button(Turtle):
 
 
 class ControlButton(Button):
-    def __init__(self, shape, wid, len, color, x, y, label_y, onclick):
+    def __init__(self, label, shape, wid, len, color, x, y, label_y, onclick):
         super().__init__(shape, wid, len, color, x, y, onclick)
         self.toggle = False 
-        
-    def writeSome():
-        pass 
+        self.goto(x, label_y)
+        self.write(f'{label}', align='center', font=("Courier", 18, "normal"))
+        self.goto(x, y)
+
         
 class ReverseButton(Button):
     def __init__(self, shape, wid, len, color, x, y, onclick, angle, width):
